Gradient based PMC working pretty well with fixed learning rate

distributions/distributions.py
@@ -1,12 +1,11 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
 import numpy.random as npr
-from numpy import exp, log
 from numpy.linalg import inv, cholesky, det
 from scipy.special import multigammaln
 from scipy.stats import chi2
 import scipy.stats as stats
-from linalg import pdinv, ensure_2d
+from linalg import pdinv
 
 #some functions taken from https://gist.github.com/jfrelinger/2638485
 
@@ -93,6 +92,9 @@ class mvnorm(object):
     def logpdf(self, x):
         return self.freeze.logpdf(x)
     
+    def logpdf_grad(self, x):
+        return self.Ki.dot(np.atleast_1d(x) - self.mu)
+    
     def rvs(self, *args, **kwargs):
         return self.freeze.rvs(*args, **kwargs)
     
@@ -146,3 +148,27 @@ class invwishart(object):
     def logpdf(self, x):
         return invwishart_logpdf(x, self.K0, self.nu0)
         
+#################################
+        
+def test_invwishart_logpdf():
+    # values from R-package bayesm, function lndIWishart(6.1, a, a)
+    a = 4 * np.eye(5)
+    assert(abs(invwishart_logpdf(a,a,6.1) + 40.526062) < 1*10**-5)
+    
+    a = np.eye(5) + np.ones((5,5))
+    assert(abs(invwishart_logpdf(a,a,6.1) + 25.1069258) < 1*10**-6)
+    
+    a = 2 * np.eye(5)
+    assert(abs(invwishart_logpdf(a,a,6.1) + 30.12885519) < 1*10**-7)
+
+    
+if __name__ == '__main__':
+    npr.seed(1)
+    nu = 5
+    a = np.array([[1,0.5,0],[0.5,1,0],[0,0,1]])
+    #print invwishart_rv(nu,a)
+    x = np.array([ invwishart_rv(nu,a) for i in range(20000)])
+    nux = np.array([invwishart_prec_rv(nu,a) for i in range(20000)])
+    print(x.shape)
+    print(np.mean(x,0),"\n", inv(np.mean(nux,0)))
+    #print inv(a)/(nu-a.shape[0]-1)

evidence.py
@@ -13,7 +13,8 @@ from numpy.linalg import inv
 
 import numpy as np
 import scipy.stats as stats
-from linalg import ensure_2d
+
+
 
 def centering_matr(n):
     return np.eye(n) - 1./n * np.ones((n,n))
@@ -52,7 +53,7 @@ def analytic_postparam_logevidence_mvnorm_known_K_li(D, mu_pr, K_pr, K_li):
     Ki_pr = inv(K_pr)
     Ki_li = inv(K_li)
     num_obs = D.shape[0]
-    mu_empirical = ensure_2d(D.mean(0))
+    mu_empirical = np.atleast_2d(D.mean(0))
     mu_empirical.shape = (np.prod(mu_empirical.shape),)
     mu_pr.shape = (np.prod(mu_pr.shape),)
     

mvnorm_example.py
@@ -24,18 +24,27 @@ import halton
 import synthdata
 from plotting import plot_var_bias_mse
 from evidence import importance_weights, analytic_postparam_logevidence_mvnorm_known_K_li, evidence_from_importance_weights
-from distributions import norm_invwishart, invwishart_logpdf, invwishart_rv, invwishart
+from distributions import norm_invwishart, invwishart_logpdf, invwishart_rv, invwishart, sureevent
 from distributions import mvnorm
 #from scipy.stats import norm as mvnorm
 
 import estimator_statistics as eststat
-from linalg import ensure_2d
 
 
+def grad_imp_sample_params_known_K_li(num_samples, D, prior, K_li):
+    rval = []
+    mu = np.atleast_2d(prior.rvs())
+    def llhood():
+        return mvnorm(mu, K_li).logpdf(D).sum()
+    for i in range(num_samples):
+        print("Posterior sample", i)
+        slice_sample_all_components(mu, llhood, prior)        
+        rval.append(mu.copy())
+    return np.array(rval)
 
 def sample_params_known_K_li(num_samples, D, prior, K_li):
     rval = []
-    mu = ensure_2d(prior.rvs())
+    mu = np.atleast_2d(prior.rvs())
     def llhood():
         return mvnorm(mu, K_li).logpdf(D).sum()
     for i in range(num_samples):
@@ -47,8 +56,8 @@ def sample_params_known_K_li(num_samples, D, prior, K_li):
 def sample_params_unknown_K_li(num_samples, D, K_pr, nu_pr, mu_pr, kappa_pr):
     rval_K = []
     rval_mu = []
-    K = ensure_2d(invwishart_rv(K_pr, nu_pr))
-    mu = ensure_2d(mvnorm(mu_pr, K / kappa_pr).rvs())
+    K = np.atleast_2d(invwishart_rv(K_pr, nu_pr))
+    mu = np.atleast_2d(mvnorm(mu_pr, K / kappa_pr).rvs())
     if len(mu.shape) == 0:
         mu = mu.reshape((1, 1))
         K = K.reshape((1, 1))
@@ -65,7 +74,7 @@ def sample_params_unknown_K_li(num_samples, D, K_pr, nu_pr, mu_pr, kappa_pr):
 
 
 ## Dimension of Gaussian ##
-dims = 2
+dims = 3
 num_obs = 100
 
 ## Number of posterior samples to draw ##
@@ -77,7 +86,7 @@ num_imp_samples = 10000
 
 num_datasets = 50
 
-if False:
+if True:
     dims = 1
     num_obs=10
     num_post_samples = 100
@@ -108,14 +117,13 @@ pr = mvnorm(mu_pr, K_pr)
 
 lowdisc_seq_sob = i4_sobol_generate(dims, num_imp_samples , 2).T
 
-estimator_names = ["qis(sobol)","is","priorIs"] #,"qis(halton)"
+
 est = {}
-res = {}
 num_evid_samp = np.logspace(1, np.log10(num_imp_samples), 15, base=10).astype(int)
 
 for obs_size in datasets:
-    est[obs_size] = {"an":[]}
-    for estim in estimator_names:
+    est[obs_size] = {"GroundTruth":[]}
+    for estim in ["qis(sobol)","is","priorIs"]:
         est[obs_size][estim] = []
     for ds in datasets[obs_size]:
         D = ds["obs"]
@@ -133,7 +141,7 @@ for obs_size in datasets:
         ((mu_post, K_post, Ki_post),
          evid) = analytic_postparam_logevidence_mvnorm_known_K_li(D, mu_pr, K_pr, K_li)
         #print("Analytic",mu_post, K_post, "\nFit", param_fit,"\n")
-        est[obs_size]["an"].append(evid)
+        est[obs_size]["GroundTruth"].append(evid)
         
         
         
@@ -143,10 +151,14 @@ for obs_size in datasets:
             return np.array([mvnorm(mean, K_li).logpdf(D).sum()
                            for mean in posterior_samples])
         
+        qis_samples = fit.ppf(lowdisc_seq_sob).reshape((num_imp_samples, dims))
         (qis_sob_w, qis_sob_w_norm) = importance_weights(D, llhood_func, pr, fit,
-                                                         fit.ppf(lowdisc_seq_sob).reshape((num_imp_samples, dims)))
+                                                         qis_samples)
         est[obs_size]["qis(sobol)"].append(evidence_from_importance_weights(qis_sob_w, num_evid_samp))
+        
         ## draw standard importance samples
+        
+        
         (is_w, is_w_norm) = importance_weights(D, llhood_func, pr, fit,
                                                fit.rvs(num_imp_samples).reshape((num_imp_samples, dims)))
         est[obs_size]["is"].append(evidence_from_importance_weights(is_w, num_evid_samp))
@@ -160,34 +172,10 @@ for obs_size in datasets:
     for key in est[obs_size]:
         est[obs_size][key] = np.array(est[obs_size][key])
         
-    res[obs_size] = {"bias^2":{}, "var": {}, "mse":{}, "bias^2{ }(relat)":{}, "var{ }(relat)": {}, "mse{ }(relat)":{}}
     
-    # now calculate bias, variance and mse of estimators when compared
-    # to analytic evidence
-    for estim in estimator_names:
-        estimate = est[obs_size][estim]
-        analytic = est[obs_size]["an"].reshape((len(est[obs_size]["an"]), 1))
-        est_rel = estimate - analytic
-        
-        bias2 = eststat.logbias2exp(analytic, estimate, axis = 0)
-        bias2_rel = eststat.logbias2exp(ensure_2d(0), est_rel, axis = 0)
-        var = eststat.logvarexp(estimate, axis = 0)
-        var_rel = eststat.logvarexp(est_rel, axis = 0)
-        mse = eststat.logmseexp(analytic, estimate, axis = 0)
-        mse_rel = eststat.logmseexp(ensure_2d(0), est_rel, axis = 0)
-        
-        res[obs_size]["bias^2"][estim] = bias2.flat[:]
-        res[obs_size]["bias^2{ }(relat)"][estim] = bias2_rel.flat[:]
-        res[obs_size]["var"][estim] =  var.flat[:]
-        res[obs_size]["var{ }(relat)"][estim] =  var_rel.flat[:]
-        res[obs_size]["mse"][estim] =  mse.flat[:]
-        res[obs_size]["mse{ }(relat)"][estim] =  mse_rel.flat[:]
-        #print(eststat.logsubtrexp(eststat.logaddexp(bias2, var)[0], mse)[0],"\n",
-        #      eststat.logsubtrexp(logsumexp(np.vstack((bias2, var)), 0), mse)[0])
-        decomp_err = eststat.logmeanexp(eststat.logsubtrexp(logsumexp(np.vstack((bias2, var)), 0), mse)[0])[0]
-      
-        if decomp_err >= -23: # error in original space >= 1e-10 
-            print("large mse decomp error, on average", decomp_err)
+
+
+res = eststat.logstatistics(est)
         
 
 